24e76292eee53c78f39064040284ddebd5482bdc
Syed Zain Anwer <zainanwer24@gmail.com>
2024-01-01 06:05:05 +0100

    completed the project

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Product
 
 def index(request): # its a HTTP request in the params that the browser would send to server
-    return HttpResponse("Hello World!")
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products}) # key-value pair in 3rd param
 
 def new(request):
     return HttpResponse("New Products")
